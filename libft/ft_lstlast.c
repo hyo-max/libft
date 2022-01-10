72280518065bdddb257d134f0f47dd731d3ef658
hyo-max <yula1147@yahoo.com>
2022-01-10 23:33:47 +0900

    lstlast:

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: hyojpark <hyojpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 14:46:10 by hyojpark          #+#    #+#             */
-/*   Updated: 2022/01/09 14:49:03 by hyojpark         ###   ########.fr       */
+/*   Updated: 2022/01/10 23:32:34 by hyojpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 t_list *ft_lstlast(t_list *lst)
 {
-	
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
